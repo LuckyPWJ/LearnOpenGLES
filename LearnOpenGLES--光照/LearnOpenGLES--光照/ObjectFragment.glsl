c651528bd5fe5fa80eb3148d4f8cc6e7a975ebd5
pwj <kissbabypwj@sina.com>
2020-04-09 22:25:56 +0800

    增加光照

--- LearnOpenGLES--光照/LearnOpenGLES--光照/ObjectFragment.glsl
+++ LearnOpenGLES--光照/LearnOpenGLES--光照/ObjectFragment.glsl
@@ -21,15 +21,18 @@ void main(){
     float abmientStrength = 0.1;
     vec3 ambientColor = abmientStrength * lightColor;
     
-//
     //漫反射光
+    //先计算光源的向量，
     vec3 lightDir = normalize(lightPos - a_fragPos);
+    //计算光源向量和法线向量的点积，计算光源对当前片段实际的漫反射影响
     float diffuse = max(dot(normal,lightDir),0.0f);
     vec3 diffuseColor = diffuse * lightColor;
 
-//    //镜面反射光
+    //镜面反射光
     float specualStrength = 0.5f;
+    //计算观察者到正方体向量
     vec3 viewDir = normalize(viewPos - a_fragPos);
+    //计算反射镜面光和法线向量的点积
     vec3 reflectDir = reflect(-lightDir,normal);
     float spec = pow(max(dot(viewDir,reflectDir),0.0),32.0);
     vec3 specualColor = specualStrength * spec * lightColor;
